9553a7fd4b4c475e5f04b389e4fd97235761fdd4
USERCHI-QIG9P26\Administrator <m18720991459_1@163.com>
2018-03-04 22:55:21 +0800

    update

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ app.all("*",function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With, accept, origin, Content-Type");
     res.header("Access-Control-Allow-Credentials", "true");
-    // res.header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
-    // res.header("Content-Type", "multipart/form-data;charset=utf-8");
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
