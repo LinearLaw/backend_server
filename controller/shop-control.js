fd3f2732f17eddcac4546455f03edabc2f23b769
LinearLiu <linear.liu@cloudaxis.com>
2018-03-05 17:58:08 +0800

    add edit shop and get shop module

--- controller/shop-control.js
+++ controller/shop-control.js
@@ -57,3 +57,94 @@ exports.addShop = (req,res)=>{
     })
   }
 }
+
+exports.getShop = (req,res)=>{
+  try{
+    new Promise((resolve)=>{
+        User.find({"userId":req.session.UID},function(err,result){
+          if(err){
+            res.send({
+              status:2,
+              content:"server error , try again"
+            })
+          }
+          if(result.length <= 0){
+            res.send({
+                status:3,
+                content:"user not exist"
+            })
+          }
+          if(req.session.UID != result[0]["userId"]){
+            res.send({
+              status:4,
+              content:"server error"
+            })
+          } 
+          res.send({
+              status:1,
+              content:"success",
+              data:result[0]
+          })
+        })
+    })
+  }catch(err){
+    console.log(err);
+    res.send({
+      status:-1,
+      content:err
+    })
+  }
+}
+
+exports.editShop = (req,res)=>{
+    try{
+    new Promise((resolve)=>{
+        let form = new formidable.IncomingForm();
+        form.parse(req,(err,fields,files)=>{
+            User.find({"userId":req.session.UID},function(err,result){
+              if(err){
+                res.send({
+                  status:2,
+                  content:"server error , try again"
+                })
+              }
+              if(result.length <= 0){
+                res.send({
+                    status:3,
+                    content:"user not exist"
+                })
+              }
+              if(req.session.UID != result[0]["userId"]){
+                res.send({
+                  status:4,
+                  content:"server error"
+                })
+              } 
+              let shopObj = {
+                userId      :   req.session.UID,
+                shopId      :   result[0]["shopId"],                   //店铺id
+                shopname    :   fields.shopname,
+                createDate  :   new Date().getTime(),
+                shopLogo    :   "",
+                shopDetail  :   fields.shopDetail,
+                shoptype    :   fields.shoptype,      //店铺类别，0：个人店铺 or 1：企业店铺
+                multishop   :   fields.multishop,
+                products    :   []
+              }
+              //待补完
+              //resolve(shopObj)
+              res.send({
+                      status:1,
+                      content:"success"
+              })
+            })
+        })
+    })
+  }catch(err){
+    console.log(err);
+    res.send({
+      status:-1,
+      content:err
+    })
+  }
+}
