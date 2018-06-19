b08ba34dbe9a0dd7b1d5a789e3799c9e1860e966
LinearLaw <17817363770@163.com>
2018-06-19 16:14:20 +0800

    update router

--- router/router.js
+++ router/router.js
@@ -1,27 +1,12 @@
-const config = require("../config/config.js")
+const express = require("express");
+const router = express.Router();
+
+const config = require("../config/config.js");
 const userControl = require("../controller/user-control.js");
 const shopControl = require("../controller/shop-control.js");
 
-//user-service
-exports.userSignup = userControl.userSignup;
-exports.userLogin = userControl.userLogin;
-
-//shop-service
-exports.addShop = function(req,res){
-  if(!config.forbidden){
-    return;
-  }else{
-    shopControl.addShop(req,res);
-  }
-}
-exports.editShop = function(req,res){
-  if(!config.forbidden){
-    return;
-  }else{
-    shopControl.editShop(req,res);
-  }
-}
-exports.getShop = function(req,res){
+//登录过滤
+function filter(req,res,cb){
   if(!config.forbidden){
     res.send({
       status:-1,
@@ -29,6 +14,17 @@ exports.getShop = function(req,res){
     })
     return;
   }else{
-    shopControl.getShop(req,res);
-  } 
+    cb(req,res);
+  }
 }
+
+//user-service
+router.post("/dosignup" , userControl.userSignup);
+router.post("/dologin"  , userControl.userLogin);
+
+//shop-service
+router.get( "/dogetshop"   , filter(req , res , shopControl.getShop));
+router.post("/doeditshop"  , filter(req , res , shopControl.editShop));
+router.post("/doaddshop"   , filter(req , res , shopControl.addShop));
+
+module.exports = router;
